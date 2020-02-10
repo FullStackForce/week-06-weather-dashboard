8e079cb1b8ddbef74cb3b6be7af955612ff9bbb0
Full Stack <fullstack@fullstacks-MacBook-Pro.local>
2020-02-10 11:58:15 -0500

    adding codes in js, formula to converts the temp, show 5 day forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,5 +24,50 @@ $("button").on("click", function () {
 
     });
 
+    var fiveDayURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey;
+
+    $.ajax({
+        url: fiveDayURL,
+        method: "GET"
+    }).then(function (responseTwo) {
+
+        // Converts the temperature
+        var tempOneF = (responseTwo.list[4].main.temp - 273.15) * 1.80 + 32;
+        var tempOne = tempOneF.toFixed(2);
+
+        var tempTwoF = (responseTwo.list[12].main.temp - 273.15) * 1.80 + 32;
+        var tempTwo = tempTwoF.toFixed(2);
+
+        var tempThreeF = (responseTwo.list[20].main.temp - 273.15) * 1.80 + 32;
+        var tempThree = tempThreeF.toFixed(2);
+
+        var tempFourF = (responseTwo.list[28].main.temp - 273.15) * 1.80 + 32;
+        var tempFour = tempFourF.toFixed(2);
+
+        var tempFiveF = (responseTwo.list[36].main.temp - 273.15) * 1.80 + 32;
+        var tempFive = tempFiveF.toFixed(2);
+
+        $("#day-1").html("<h6>" + responseTwo.list[4].dt_txt + "</h6>")
+        $("#day-1").append("<p>" + "Temp: " + tempOne + "</p>")
+        $("#day-1").append("<p>" + "Humidity: " + responseTwo.list[4].main.humidity + "</p>")
+
+        $("#day-2").html("<h6>" + responseTwo.list[12].dt_txt + "</h6>")
+        $("#day-2").append("<p>" + "Temp: " + tempTwo + "</p>")
+        $("#day-2").append("<p>" + "Humidity: " + responseTwo.list[12].main.humidity + "</p>")
+
+        $("#day-3").html("<h6>" + responseTwo.list[20].dt_txt + "</h6>")
+        $("#day-3").append("<p>" + "Temp: " + tempThree + "</p>")
+        $("#day-3").append("<p>" + "Humidity: " + responseTwo.list[20].main.humidity + "</p>")
+
+        $("#day-4").html("<h6>" + responseTwo.list[28].dt_txt + "</h6>")
+        $("#day-4").append("<p>" + "Temp: " + tempFour + "</p>")
+        $("#day-4").append("<p>" + "Humidity: " + responseTwo.list[28].main.humidity + "</p>")
+
+        $("#day-5").html("<h6>" + responseTwo.list[36].dt_txt + "</h6>")
+        $("#day-5").append("<p>" + "Temp: " + tempFive + "</p>")
+        $("#day-5").append("<p>" + "Humidity: " + responseTwo.list[36].main.humidity + "</p>")
+
+    });
+
 
 })
